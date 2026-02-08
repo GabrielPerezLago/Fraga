44fc2cbee77cb191944327552fed9c838d699c35
Gabriel <gabrielperezlago.dev@gmail.com>
2026-02-08 18:56:24 +0100

    v26.02.08 API

---
+++ api/FragaAPI/src/main/kotlin/com/gabriel/fraga/fragaapi/FragaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.gabriel.fraga.fragaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FragaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FragaApiApplication>(*args)
+}
---
+++ api/FragaAPI/src/test/kotlin/com/gabriel/fraga/fragaapi/FragaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabriel.fraga.fragaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FragaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
